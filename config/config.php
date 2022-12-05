551b97a1da5632dc52cbe8dc5f25bd52c101c9ba
Ako-56 <70592757+Ako-56@users.noreply.github.com>
2022-12-05 11:53:58 +0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+$server= "127.0.0.1:3306";
+$username = "root";
+$password = "Routesoft-Admin";
+$dbname = "ress";
+
+
+// Create connection
+$conn = mysqli_connect($server, $username,$password,$dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
